616064244d25f5af9033e1ec38e6344c4d782c27
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-06 17:59:33 -0700

    created a new

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 const app = express();
-const passportSetup = require('./config/passport-setup')
+const passportSetup = require('./config/passport-setup');
 
 const PORT = process.env.PORT || 3000
 
+mongoose.connect(keys.mongodb.dbURI, { 
+    useNewUrlParser: true
+     
+} ,() => console.log('connected to db'))
+    
 app.set('view engine', 'ejs');
 app.use('/auth', require('./routes/auth-routes'));
 
